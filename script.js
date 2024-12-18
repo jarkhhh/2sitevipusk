fd28f4a0e0f23b3c0c1076db8eb4ad0b696417ed
jarkhhh <121623503+jarkhhh@users.noreply.github.com>
2024-12-18 20:52:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 changeBG()
 function changeBG(){
     document.body.style.backgroundColor = localStorage.getItem("bgcolor");
-    console.log(obj.value);
+    
 }
 const obj =document.getElementById("colparm");
 function saveBgCol(){
